42f66b35dad8d0b7d01f645b8e0533d65a9f9867
JBaeza27 <107650433+JBaeza27@users.noreply.github.com>
2024-10-12 13:49:31 -0500

    Spring Boot skeleton

---
+++ email-encryption/src/main/java/com/example/email/encryption/EmailEncryptionApplication.java
@@ -0,0 +1,13 @@
+package com.example.email.encryption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailEncryptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailEncryptionApplication.class, args);
+	}
+
+}
---
+++ email-encryption/src/test/java/com/example/email/encryption/EmailEncryptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.email.encryption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailEncryptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
