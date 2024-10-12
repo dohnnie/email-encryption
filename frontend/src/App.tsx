7f2ff175cc58fec16f06f02241779a50e657ba29
Johnny Ngo <jtn.tria@gmail.com>
2024-10-12 17:33:51 -0500

    Layout of website done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup, } from "@/components/ui/resizable"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function MailContents() {
+ return (
+  <div className="bg-purple-700">
+      This is the mail contents
+  </div>
+ ); 
 }
 
-export default App
+function Inbox() {
+  return(
+    <div className="bg-teal-600">
+      This is the inbox
+    </div>
+  );
+}
+
+function Sidebar() {
+  return (
+    <div className="flex flex-col min-h-10 bg-yellow-300">
+      This is the sidebar
+    </div>
+  );
+}
+
+
+function App()  {
+    return(
+    <div className="grid grid-cols-3 min-h-screen bg-pink-950" >
+      <Sidebar className="" />
+      <Inbox />
+      <MailContents />
+    </div>
+   );
+}
+
+export default App;
